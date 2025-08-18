1ba3e96f771ce3bfe28a1959e4347f5be37f1f91
a.developer <developer@developer.ru>
2025-08-18 14:31:24 +0300

    chore(controller): анимация для группы кубиков

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -7,3 +7,9 @@ export const enum COLORS {
     WHITE = "#FFFFFF",
     BLACK = "#000000"
 }
+
+export const CubColors = [0xed7272, 0xfc6e3a, 0xf2e746, 0x89e62c, 0x17e6d4, 0x325be3, 0xb24dd6, 0xe31092, 0xbf001a];
+export const GROUP_SCALES = {
+    MIN: .6,
+    MAX: 1.1
+}
--- src/three/scenes/contoller.ts
+++ src/three/scenes/contoller.ts
@@ -1,51 +1,77 @@
 import * as THREE from 'three'
+import {CubColors, GROUP_SCALES} from "@constants/constants.ts";
 
 export function controller(el) {
   const scene = new THREE.Scene();
-  const size = {w: window.innerWidth, h: window.innerHeight}
-  console.log('scene', scene)
+  const size = {w: window.innerWidth, h: window.innerHeight};
 
   //Ось
   const axesHelper = new THREE.AxesHelper(3);
-  scene.add(axesHelper)
+  scene.add(axesHelper);
 
   //Объект
   const group = new THREE.Group();
-  scene.add(group)
+  scene.add(group);
+  const meshes = [];
+
+  let colorId = 0;
   const geometry = new THREE.BoxGeometry(1, 1, 1);
-  const material = new THREE.MeshBasicMaterial({color: 'green', wireframe: true})
-  const mesh1 = new THREE.Mesh(geometry, material);
-  const mesh2 = new THREE.Mesh(geometry, material);
-  const mesh3 = new THREE.Mesh(geometry, material);
-
-  mesh1.position.set(-1.5, 0, 0);
-  mesh3.position.set(1.5, 0, 0);
-
-  group.add(mesh1, mesh2, mesh3)
+  for(let x=-1.2; x<=1.2; x+=1.2) {
+    for(let y=-1.2; y<=1.2; y+=1.2) {
+      const mesh = new THREE.Mesh(geometry, new THREE.MeshBasicMaterial({
+        color: CubColors[colorId], wireframe: true
+      }));
+      mesh.position.set(x,y,0)
+      mesh.scale.set(.55,.55,.55)
+      meshes.push(mesh);
+      colorId++;
+    }
+  }
+  group.add(...meshes)
 
 
   //Камера
   const camera = new THREE.PerspectiveCamera(75, size.w / size.h);
-  scene.add(camera)
-  camera.position.set(0, 2, 3);
-  camera.lookAt(group.position);
+  scene.add(camera);
+  camera.position.set(0, 0, 3);
 
   //Рендер
-  const renderer = new THREE.WebGLRenderer({canvas: el.current})
-  renderer.setSize(size.w, size.h)
-  renderer.render(scene, camera)
+  const renderer = new THREE.WebGLRenderer({canvas: el.current});
+  renderer.setSize(size.w, size.h);
+  renderer.render(scene, camera);
 
 
   const clock = new THREE.Clock();
-  const tick = () => {
-    const elapsedTime = clock.getElapsedTime();
-    group.rotation.x = elapsedTime*3;
-    camera.position.x = Math.sin(elapsedTime)
-    camera.position.y = Math.cos(elapsedTime)
-    camera.lookAt(group.position)
+  let grow = false;
 
-    renderer.render(scene, camera)
-    window.requestAnimationFrame(tick)
+  const tick = () => {
+    const delta = clock.getDelta();
+    const elapsedTime = clock.getElapsedTime();
+
+    meshes.forEach((el, id) => {
+      const mult = id%2===0 ? 1 : -1;
+      el.rotation.x += delta * mult;
+      el.rotation.y += delta * mult * .4;
+    })
+
+    camera.position.x = Math.cos(elapsedTime);
+    camera.position.y = Math.sin(elapsedTime);
+    camera.lookAt(new THREE.Vector3(0,0,0))
+
+    const mult = grow ? 1 : -1;
+    const speed = .1;
+    group.scale.x += delta * mult * speed;
+    group.scale.y += delta * mult * speed;
+    group.scale.z += delta * mult * speed;
+
+    if(grow && group.scale.x >= GROUP_SCALES.MAX) {
+      grow = false;
+    } else if (group.scale.x <= GROUP_SCALES.MIN) {
+      grow = true;
+    }
+
+    renderer.render(scene, camera);
+    window.requestAnimationFrame(tick);
   }
 
   tick();
