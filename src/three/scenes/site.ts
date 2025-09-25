ffb55f8954b2eff496dd3261aca5eb9ec0367a86
a.developer <developer@developer.ru>
2025-09-26 00:29:40 +0300

    chore(site): расставлены точки, реагирующие на кота

--- src/three/scenes/site.ts
+++ src/three/scenes/site.ts
@@ -1,4 +1,5 @@
 import * as THREE from 'three'
+import {gsap} from "gsap";
 import {GeometryPack} from "@constants/constants.ts";
 import {OrbitControls} from "three/examples/jsm/controls/OrbitControls";
 import Stats from 'three/examples/jsm/libs/stats.module.js'
@@ -13,6 +14,15 @@ export class Controller {
   private camera: THREE.PerspectiveCamera;
   private renderer: THREE.WebGLRenderer;
   private orbitControls: OrbitControls;
+
+  positions = [
+    {position: new THREE.Vector3(0, 10, -20)},
+    {position: new THREE.Vector3(20, 10, 0)},
+    {position: new THREE.Vector3(0, 10, 20)},
+    {position: new THREE.Vector3(-20, 10, 0)},
+  ]
+  spheres: THREE.Object3D[] = [];
+  currentIndex: number | null = null;
 
   private modelController;
 
@@ -57,17 +67,30 @@ export class Controller {
 
 
   createObjects() {
+    const spheres = this.positions.map(({position}) => {
+      const sphere = new THREE.Mesh(
+        new THREE.SphereGeometry(1, 24, 24),
+        new THREE.MeshStandardMaterial({color: 'green'})
+      )
+      this.spheres.push(sphere)
+      sphere.position.set(...position)
+      sphere.castShadow = true;
+      return sphere
+    })
+
     const texture = new THREE.TextureLoader().load('src/assets/stone-texture.jpg');
     texture.wrapS = THREE.RepeatWrapping;
     texture.wrapT = THREE.RepeatWrapping;
-    texture.repeat.set(20,4);
+    texture.repeat.set(20, 4);
 
     const mesh = new THREE.Mesh(
       GeometryPack[0],
       new THREE.MeshStandardMaterial({map: texture}));
     mesh.position.set(0, 0, 0);
-    mesh.rotation.set(-Math.PI/2, 0, 0);
+    mesh.rotation.set(-Math.PI / 2, 0, 0);
     mesh.receiveShadow = true;
+
+    this.scene.add(...spheres)
     this.scene.add(mesh);
   }
 
@@ -77,14 +100,14 @@ export class Controller {
     loader.load(
       'src/models/cat/scene.gltf',
       gltf => {
-        this.modelController = new ModelController(gltf);
+        this.modelController = new ModelController(gltf, this.positions);
         this.modelController.enableShadows();
         this.modelController.addListeners();
 
         this.scene.add(this.modelController.model);
       },
       xhr => {
-        console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' )
+        console.log((xhr.loaded / xhr.total * 100) + '% loaded')
       },
       error => {
         console.log('Error: ', error)
@@ -96,7 +119,7 @@ export class Controller {
     const ambientLight = new THREE.AmbientLight(0x404040, 20);
     const directionalLight = new THREE.DirectionalLight(0x404040, 20);
 
-    directionalLight.position.set(-50, 50, 0);
+    directionalLight.position.set(-30, 50, 0);
     directionalLight.castShadow = true;
 
     directionalLight.shadow.mapSize.width = 2048;
@@ -115,8 +138,8 @@ export class Controller {
   createCamera() {
     this.camera = new THREE.PerspectiveCamera(75, this.size.w / this.size.h);
     this.scene.add(this.camera);
-    this.camera.position.set(0, 10, 30);
-    this.camera.lookAt(new THREE.Vector3(0,0, 0))
+    this.camera.position.set(0, 13, 30);
+    this.camera.lookAt(new THREE.Vector3(0, 0, 0))
   }
 
 
@@ -141,12 +164,35 @@ export class Controller {
     this.orbitControls.enableDamping = true;
   }
 
+  animateSphere(sphereIndex) {
+    if (sphereIndex === this.currentIndex) return;
+
+    // Возвращаем предыдущую сферу в нормальное состояние
+    if (this.currentIndex !== null) {
+      const previousSphere = this.spheres[this.currentIndex];
+      gsap.to(previousSphere.scale, {x: 1, y: 1, z: 1, duration: 1, ease: "elastic.out(1, 0.3)"});
+      gsap.to(previousSphere.material.color, {r: 0, g: 1, b: 0, duration: 1, ease: "power2.inOut"});
+    }
+
+    // Анимируем новую активную сферу
+    if (sphereIndex !== null) {
+      const newSphere = this.spheres[sphereIndex];
+
+      gsap.to(newSphere.scale, {x: 2, y: 2, z: 2, duration: 1, ease: "elastic.out(1, 0.3)"});
+      gsap.to(newSphere.material.color, {r: 1, g: 0, b: 0, duration: 1, ease: "power2.inOut"});
+    }
+    this.currentIndex = sphereIndex;
+  }
+
   tick() {
     this.stats.begin();
     const delta = this.clock.getDelta();
 
     if (this.modelController) {
       this.modelController.move(delta);
+
+      const sphereIndex = this.modelController.getActivePosition();
+      this.animateSphere(sphereIndex)
     }
 
     this.renderer.render(this.scene, this.camera);
--- src/utils/modelController.ts
+++ src/utils/modelController.ts
@@ -1,15 +1,25 @@
 import * as THREE from 'three'
 
 export class ModelController {
-  model;
-  mixer: THREE.AnimationMixer;
-  animations: {[key: string]: THREE.AnimationAction} = {};
+  private model;
+  private mixer: THREE.AnimationMixer;
+  private animations: {[key: string]: THREE.AnimationAction} = {};
 
-  angle: number = 0;
-  radius: number = 20;
-  isMoving: boolean = false;
+  private angle: number = 0;
+  private radius: number = 20;
+  private isMoving: boolean = false;
 
-  constructor(model) {
+  positions;
+  activePosition: number | null = null;
+
+  constructor(model, positions) {
+    this.positions = positions;
+
+    this.initModel(model);
+  }
+
+
+  initModel(model) {
     this.model = model.scene;
     this.model.castShadow = true;
     const animations = model.animations;
@@ -21,22 +31,38 @@ export class ModelController {
       this.mixer = new THREE.AnimationMixer(this.model)
 
       this.animations.walk = this.mixer.clipAction(animations[0]);
-
     }
   }
 
   move(delta) {
     if(!this.isMoving) return;
+
     if(this.animations.walk) {
       this.animations.walk.play();
+      this.checkNearPosition();
       this.updateMixer(delta)
     }
 
-    this.angle+=.005;
+    this.angle+=.007;
 
     this.model.position.x =  this.radius * Math.cos(this.angle);
     this.model.position.z = this.radius * Math.sin(this.angle);
     this.model.rotation.y = -this.angle;
+  }
+
+  checkNearPosition() {
+    let value = null;
+    this.positions.forEach((el,id) => {
+      if(this.model.position.distanceTo(el.position) < 11) {
+        value = id;
+      }
+    })
+
+    this.activePosition = value;
+  }
+
+  getActivePosition() {
+    return this.activePosition
   }
 
   enableShadows() {
@@ -57,13 +83,12 @@ export class ModelController {
   }
 
 
-
   addListeners() {
     window.addEventListener('keydown', (e)=> {
-      if(e.key==='w') this.isMoving = true;
+      if(e.key==='ArrowUp') this.isMoving = true;
     })
     window.addEventListener('keyup', (e)=> {
-      if(e.key==='w') this.isMoving = false;
+      if(e.key==='ArrowUp') this.isMoving = false;
     })
   }
 
